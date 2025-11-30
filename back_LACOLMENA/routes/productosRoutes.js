71024c084c078fb1485be553f1b832057099f722
ItaliaHernandez <italiahero99@hotmail.com>
2025-11-30 16:56:33 -0600

    Primer commit estructura del proyecto y primeras API's acerca de los productos

---
+++ back_LACOLMENA/routes/productosRoutes.js
@@ -0,0 +1,21 @@
+// routes/prodcutosRoutes.js
+const express = require('express');
+const router = express.Router();
+
+//Rutas relacionadas con lo productos
+const {
+    //ADMINITRADOR
+    getProductos,           //mostrar productos
+    getProductoId,          //mostrar producto segun id
+    postProducto,           //crear producto
+    deleteProducto,         //eliminar producto
+    putProducto             //actualizar producto
+} = require('../controllers/productosController');
+
+router.get('/', getProductos );
+router.get('/:id', getProductoId );
+router.post('/postProducto', postProducto );
+router.delete('/:id', deleteProducto );
+router.put('/:id', putProducto );
+
+module.exports = router;
---
+++ back_LACOLMENA/server.js
@@ -0,0 +1,36 @@
+// server.js
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const pool = require('./db/conexion'); // <-- Importamos la conexión
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+//RUTAS
+const productosRoutes = require('./routes/productosRoutes');
+
+app.use('/api/productos', productosRoutes);
+
+// Ruta base
+app.get('/', (req, res) => {
+    res.send(' API la_colmena funcionando correctamente');
+});
+
+// Funcion que hace una consulta de prueba mínima que
+// confirma que todo el circuito conexión → consulta → respuesta está funcionando
+async function testConnection() {
+    try {
+        const [rows] = await pool.query('SELECT 1 + 1 AS result'); //Le pide a MySQL que sume 1 + 1, y le ponga el alias result al valor obtenido
+        console.log(' Conexión a la base de datos establecida. Resultado:', rows[0].result);
+    } catch (error) {
+        console.error(' Error al conectar con la base de datos:', error.message);
+    }
+}
+
+// Iniciar servidor y probar conexión
+app.listen(PORT, async () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+    await testConnection(); // <--------------------- se ejecuta al arrancar el servidor
+});
