d5f9bacced15866efa20a611d628a6586537078b
Jesus Damasco <damascojesus1@gmail.com>
2025-12-05 15:53:55 -0600

    Prototipo de backend para cuentas

--- src/db/conexion.js
+++ src/db/conexion.js
@@ -1,15 +1,12 @@
 // db/conexion.js
-const mysql = require('mysql2/promise');
-
-const pool = mysql.createPool(process.env.DATABASE_URL);
-
-// Añadir event listeners para debug
-pool.on('connection', (connection) => {
-  console.log('Nueva conexión establecida con la BD');
+const mysql = require('mysql2/promise'); 
+const pool = mysql.createPool({ 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true, 
+    connectionLimit: 10, 
+    queueLimit: 0 
 });
-
-pool.on('error', (err) => {
-  console.error('Error en el pool de conexiones:', err);
-});
-
 module.exports = pool;
